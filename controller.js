fcfe3b495e6c3d95a71b5defb047e19ee552a560
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-27 10:22:45 -0300

    contro

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
 angular.module("IndexApp",[])
-	.controller("IndexController",funtion($scope,$http){
+	.controller("IndexController",function($scope,$http){
 		$http.get("http://http://gremiatico.cl/MikaJS/Data/Tomos.json")
 			.success(funtion(data){
 				console.log(data);

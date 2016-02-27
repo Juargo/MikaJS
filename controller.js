88fb584a7f5c778e829c483ebac8a6598f761754
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-27 12:49:57 -0300

    s

--- controller.js
+++ controller.js
@@ -4,9 +4,9 @@ angular.module("IndexApp",[])
 		$http.get("http://gremiatico.cl/MikaJS/Data/Tomos.json")
 			.success(function(data){
 				$scope.mangas = data;
-				$scope.mangas = $scope.mangas[0];
-				$scope.cant = $scope.mangas.length;
-				console.log($scope.mangas[0]);
+				//$scope.mangas = $scope.mangas[0];
+				//$scope.cant = $scope.mangas.length;
+				//console.log($scope.mangas[0]);
 			})
 			.error(function(err){
 

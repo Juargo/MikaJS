1ba791664b9ab96c1514d5566206655c33e71c90
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-27 15:45:09 -0300

    e

--- controller.js
+++ controller.js
@@ -1,4 +1,16 @@
 angular.module("IndexApp",[])
 	.controller("IndexController",function($scope,$http){
 		$http.get("Data/Tomos.json")
+		.success(function(data){
+				$scope.mangas = data;
+				$scope.mangas = $scope.mangas[0];
+				$scope.cant = $scope.mangas.length;
+				console.log($scope.mangas);
+				//$scope.mangas = $scope.mangas[0];
+				//$scope.cant = $scope.mangas.length;
+				//console.log($scope.mangas[0]);
+			})
+		.error(function(err){
+
+		});
     });

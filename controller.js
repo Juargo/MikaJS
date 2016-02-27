3667c0bb96699696067124ba5b223fba0cb8013a
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-27 16:07:33 -0300

    c

--- controller.js
+++ controller.js
@@ -3,8 +3,6 @@ angular.module("IndexApp",[])
 		$http.get("Data/Tomos.json")
 		.success(function(data){
 				$scope.mangas = data;
-				$scope.mangas = $scope.mangas[0];
-				$scope.cant = $scope.mangas.length;
 				console.log($scope.mangas);
 				//$scope.mangas = $scope.mangas[0];
 				//$scope.cant = $scope.mangas.length;

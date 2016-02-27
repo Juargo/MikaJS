4a3be75c39a1aba8d2c409f1eb166b3f7e49ed51
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-27 14:18:08 -0300

    w

--- controller.js
+++ controller.js
@@ -2,10 +2,5 @@ angular.module("IndexApp",[])
 	.controller("IndexController",function($scope,$http){
 		$scope.mangas = [];
 		$http.get("http://gremiatico.cl/MikaJS/Data/Tomos.json")
-			.success(function(data){
-
-			})
-			.error(function(err){
-
-			});
+			
 	});

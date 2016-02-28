2486a5b0c4419cb21646f22a6f7fe1d9a36e1f55
Juargo <jorgeretamal.jjra@gmail.com>
2016-02-28 17:41:40 -0300

    sss

--- controller.js
+++ controller.js
@@ -1,9 +1,18 @@
 angular.module("IndexApp",[])
+	.directive('logoImg',function(){
+		return function(scope,element,attrs){
+			attrs.$observe('logoImg',function(value){
+				element.css({
+					"background":"url("+value+")",
+				});
+			})
+		}
+	})
 	.controller("IndexController",function($scope,$http){
 		$http.get("Data/Tomos.json")
 		.success(function(data){
 				$scope.mangas = data;
-				console.log($scope.mangas)
+				console.log($scope.mangas);
 			})
 		.error(function(err){
 
